0971828685e1475328d11f0649547d5242c6099a
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2026-04-30 14:49:16 +0300

    29: replace OWNER/DSEQ columns with EXTERNAL-NAME before AGE

--- apis/resource/v1alpha1/deployment_types.go
+++ apis/resource/v1alpha1/deployment_types.go
@@ -136,13 +136,12 @@ type DeploymentStatus struct {
 // A Deployment represents an Akash Network deployment resource.
 // +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
-// +kubebuilder:printcolumn:name="DSEQ",type="string",JSONPath=".status.atProvider.deploymentId"
 // +kubebuilder:printcolumn:name="STATE",type="string",JSONPath=".status.atProvider.state"
 // +kubebuilder:printcolumn:name="PHASE",type="string",JSONPath=".status.atProvider.phase"
 // +kubebuilder:printcolumn:name="ORDERS",type="integer",JSONPath=".status.atProvider.ordersOpen"
 // +kubebuilder:printcolumn:name="BIDS",type="integer",JSONPath=".status.atProvider.bids"
 // +kubebuilder:printcolumn:name="LEASES",type="integer",JSONPath=".status.atProvider.leasesActive"
-// +kubebuilder:printcolumn:name="OWNER",type="string",JSONPath=".status.atProvider.owner"
+// +kubebuilder:printcolumn:name="EXTERNAL-NAME",type="string",JSONPath=".metadata.annotations['crossplane\\.io/external-name']"
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,akash}
