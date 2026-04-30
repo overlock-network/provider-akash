cfdf6f99e899e892636d38f28a230c74b7a32add
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2026-04-30 10:36:35 +0300

    25: rename ProviderConfig/StoreConfig group from akash.web7.md to akash.overlock.network

--- apis/v1alpha1/groupversion_info.go
+++ apis/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains the core resources of the Akash provider.
 // +kubebuilder:object:generate=true
-// +groupName=akash.web7.md
+// +groupName=akash.overlock.network
 // +versionName=v1alpha1
 package v1alpha1
 
@@ -27,7 +27,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "akash.web7.md"
+	Group   = "akash.overlock.network"
 	Version = "v1alpha1"
 )
 
